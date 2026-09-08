2268d6b2d709c6d00f77fe8c230ac04633933203
Rodrigo <rodrigogoncalvespereira2026@gmail.com>
2026-09-08 16:21:19 +0100

    feat: PWA - instalar como app no telemóvel

--- app.py
+++ app.py
@@ -129,6 +129,7 @@ CONTENT_TYPES = {
     ".html": "text/html; charset=utf-8",
     ".css": "text/css; charset=utf-8",
     ".js": "text/javascript; charset=utf-8",
+    ".json": "application/json; charset=utf-8",
     ".svg": "image/svg+xml",
     ".png": "image/png",
     ".ico": "image/x-icon",
